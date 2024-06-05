cdab7467cb6e57c60064359515ba45640127c811
herna <herna@HERNAN>
2024-06-05 15:35:19 -0300

    Agregar archivos de proyecto.

---
+++ ABMEmpleados/Models/ABMEMPLEADOSContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ABMEmpleados.Models
+{
+    public partial class ABMEMPLEADOSContext : DbContext
+    {
+        public ABMEMPLEADOSContext()
+        {
+        }
+
+        public ABMEMPLEADOSContext(DbContextOptions<ABMEMPLEADOSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//              optionsBuilder.UseSqlServer("server=localhost; database=ABMEMPLEADOS; integrated security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.Property(e => e.Apellido).HasMaxLength(100);
+
+                entity.Property(e => e.Cargo).HasMaxLength(100);
+
+                entity.Property(e => e.FechaContratacion).HasColumnType("date");
+
+                entity.Property(e => e.Nombre).HasMaxLength(100);
+
+                entity.Property(e => e.Salario).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
